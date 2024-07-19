db986306b9c789cc25856802931493ca933be176
jonas-rer <jnrenfer@gmail.com>
2024-07-19 10:31:18 +0200

    Update demand handling and episode length

---
+++ Notes/planned_demand.py
@@ -0,0 +1,32 @@
+episode = 30
+length = 30
+lead_time = 3
+done = False
+
+planned_demand = [0, 0, 0, 0, 0, 15,
+                0, 0, 0, 0, 0, 15,
+                0, 0, 0, 0, 0, 15,
+                0, 0, 0, 0, 0, 15,
+                0, 0, 0, 0, 0, 15,
+                0, 0, 0, 0, 0, 15]
+
+expected_demand = 0
+
+while done == False:
+
+    # Get the current day
+    i = length - episode
+
+    if i + lead_time < len(planned_demand):
+        expected_demand = planned_demand[i+lead_time]
+        print(f"Expected demand for day {i+1} is {expected_demand}")
+    else:
+        print("Index out of range")
+
+    episode -= 1
+
+    # Check if episode is done
+    if episode <= 0: 
+        done = True
+    else:
+        done = False
